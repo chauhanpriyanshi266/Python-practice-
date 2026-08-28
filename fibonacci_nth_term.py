ef598e8dece6112b71232bcca8451f43626ab577
chauhanpriyanshi266 <chauhanpriyanshi266@gmail.com>
2026-08-28 23:24:26 +0530

    Update fibonacci_nth_term.py

--- fibonacci_nth_term.py
+++ fibonacci_nth_term.py
@@ -1,12 +1,16 @@
+# Program to find the nth term of the Fibonacci series
 n = int(input("Enter number : "))
+# The first and second fibonacci terms are 1
 if(n == 1 or n == 2):
     print("Fibonacci of",n,"th term is :",1)
 else:
     a = 1
     b = 1
     fibo_sum = 0
+# Calculate the Fibonacci terms from 3rd to nth
     for i in range(3,n+1):
         fibo_sum = a + b 
+# Move to the next two terms 
         a = b 
         b = fibo_sum
     print(" Fibonacci of",n,"th tern is :",fibo_sum)
