11ec7051ac3096e70b82d08b328737a69816056c
chauhanpriyanshi266 <chauhanpriyanshi266@gmail.com>
2026-09-09 13:19:52 +0530

    Create maximum_element_in_list.py

---
+++ maximum_element_in_list.py
@@ -0,0 +1,10 @@
+# Program to find the maximum element in a list
+
+lst = []
+n = int(input("Enter n: "))
+
+# The loop runs n times
+for i in range(n):
+    lst.append(int(input("Enter x: ")))
+
+print("Maximum =", max(lst))
