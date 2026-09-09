2fdb02955affc75ea653fb74a5c3227db5f842b6
chauhanpriyanshi266 <chauhanpriyanshi266@gmail.com>
2026-09-09 13:12:04 +0530

    Create input_five_numbers_in_list.py

---
+++ input_five_numbers_in_list.py
@@ -0,0 +1,11 @@
+# Program to input 5 numbers, store them in a list and print the list
+
+lst = []
+
+lst.append(int(input("Enter n1: ")))
+lst.append(int(input("Enter n2: ")))
+lst.append(int(input("Enter n3: ")))
+lst.append(int(input("Enter n4: ")))
+lst.append(int(input("Enter n5: ")))
+
+print(lst)
