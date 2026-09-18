7e7f0d2520d44e3c96d1cfff530df0791b2cb342
chauhanpriyanshi266 <chauhanpriyanshi266@gmail.com>
2026-09-18 18:07:32 +0530

    Update find_missing_number.py

--- find_missing_number.py
+++ find_missing_number.py
@@ -8,6 +8,6 @@ for i in range(n):
     lst.append(int(input("Enter element: ")))
 
 # Find the missing number from 1 to n + 1
-for i in range(1, n + 2):
+for i in range(1, n + 1):
     if i not in lst:
         print("Missing number:", i)
